45879f7f240ae03097ff87af9f433323004a780b
Kal-El-pt2 <arivwalia@gmail.com>
2025-06-12 09:07:44 +0530

    fix: value used in test changed

--- src/api/credits.rs
+++ src/api/credits.rs
@@ -53,6 +53,6 @@ mod tests{
         let token_auth = std::env::var("TEST_TOKEN_AUTH").expect("TEST_TOKEN_AUTH environment variable not set");
         let mut client = create_client(&url).await.expect("Failed to create API client. Ensure CHIRAL_STAGING_API_URL is valid and the client can be created.");
         let points_value = get_credit_points(&mut client, &email, &token_auth).await.expect("Failed to get credit points. Check network, API URL, and credentials.");
-        assert_eq!(points_value.as_f64().expect("Credit points received are not an i64 number."),3000.00);
+        assert_eq!(points_value.as_f64().expect("Credit points received are not an f64 number."),7980.002);
     }
 }
